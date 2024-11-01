4908ff8d545a78685967c8e5a4decbad0cbbe5b9
boorownie <probitanima11@gmail.com>
2024-11-01 19:59:47 +0900

    step1 - aop

--- src/main/java/nextstep/security/authorization/CheckAuthenticationFilter.java
+++ src/main/java/nextstep/security/authorization/CheckAuthenticationFilter.java
@@ -12,9 +12,15 @@ import java.io.IOException;
 import java.util.Set;
 
 public class CheckAuthenticationFilter extends OncePerRequestFilter {
+    private static final String DEFAULT_REQUEST_URI = "/members";
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        if (!DEFAULT_REQUEST_URI.equals(request.getRequestURI())) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication == null || !authentication.isAuthenticated()) {
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
---
+++ src/main/java/nextstep/security/authorization/ForbiddenException.java
@@ -0,0 +1,8 @@
+package nextstep.security.authorization;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.FORBIDDEN)
+public class ForbiddenException extends RuntimeException {
+}
